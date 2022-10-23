d1cae319f864d6788a48ab70f80453c53a276d68
Night_Spark <Night_Spark@DESKTOP-B2K2G54>
2022-10-23 21:52:13 +0300

    Add project files.

---
+++ Dz_19.10/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "Header.h"
+#include "Header1.h"
+using namespace std;
+
+int main()
+{
+	Book obj1("Apple", "Maks", 1095, "NYC", "Employer");
+	Book obj2("Micr", "Paul", 1587, "London", "Developer");
+}
